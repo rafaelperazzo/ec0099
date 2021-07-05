3a9bc3ffbe38a5b332a5517494f1aa6468c5c74b
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-07-05 09:06:22 -0300

    05/07/2021 - 09:08

--- codigo/23_n_3.py
+++ codigo/23_n_3.py
@@ -1,7 +1,5 @@
-m = int(input('Digite m: '))
-for n in range (1,m,1):
-    ultimo_impar = (n*(n+1))-1
-    primeiro_impar = ultimo_impar - ((n-1)*2)
-    print(n)
-    for impar in range(primeiro_impar,ultimo_impar+1,2):
-        print(impar)
+n = int(input('Digite n: '))
+ultimo_impar = (n*(n+1))-1
+primeiro_impar = ultimo_impar - ((n-1)*2)
+for impar in range(primeiro_impar,ultimo_impar+1,2):
+    print(impar)
